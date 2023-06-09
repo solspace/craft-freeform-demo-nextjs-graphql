7827ce855e02889808b51bdd2beefabc6fab5edd
Sean Delaney <sean@solspace.com>
2023-06-09 10:44:37 +0100

    Added more refinements

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
-import ContactForm from '@/app/contact-form';
+import QuoteForm from '@/app/quote-form';
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24">
-      <h2 className="mb-3 text-2xl font-semibold">NextJS + GraphQL demo for Freeform 4.x for Craft</h2>
-      <ContactForm />
+      <h2 className="mb-1 text-lg font-semibold text-left">Freeform 4.x for Craft</h2>
+      <h1 className="mb-5 pb-2 text-2xl font-semibold text-left">NextJS + GraphQL demo</h1>
+      <QuoteForm />
     </main>
   );
 };
